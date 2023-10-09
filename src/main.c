e31ceaa2e4daee0c7442735ab132588aeb65f854
Luis Ignacio Pastor Cristobal <lpastor-@c2r9s3.42madrid.com>
2023-10-09 13:03:59 +0200

    Added arguments tests and a instruction tester

--- src/main.c
+++ src/main.c
@@ -6,34 +6,19 @@
 /*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 08:30:32 by lpastor-          #+#    #+#             */
-/*   Updated: 2023/10/06 11:19:07 by lpastor-         ###   ########.fr       */
+/*   Updated: 2023/10/09 12:32:14 by lpastor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "../libft/libft.h"
+#include "../include/stack.h"
+#include "../include/instructions.h"
 
 void leaks(void)
 {
 	system("leaks -q push_swap");
-}
-
-void	ft_lstprint(t_list *lst, void (*f)(void*))
-{
-	size_t	count;
-
-	if (!lst | !f)
-		return ;
-	count = 0;
-	while (lst)
-	{
-		printf("(Node %zu): ", count);
-		f(lst->content);
-		printf("\n");
-		lst = lst->next;
-		count++;
-	}
 }
 
 void	int_print(void *elem)
@@ -48,6 +33,7 @@ static int	*check_arguments(int argc, char **argv)
 	int		*array;
 	int		flag;
 	int		index;
+	int		check;
 
 	index = 1;
 	array = (int *) malloc((argc - 1) * sizeof(int));
@@ -57,32 +43,36 @@ static int	*check_arguments(int argc, char **argv)
 	{
 		array[index-1] = ft_strict_atoi(argv[index], &flag);
 		if (flag < 0)
+			return (free(array), NULL);
+		check = 0;
+		while (check < (index - 1))
 		{
-			free(array);
-			return (NULL);
+			if (array[check] == array[index - 1])
+				return (free(array), NULL);
+			check++;
 		}
 		index++;
 	}
 	return (array);
 }
 
-static t_list	*get_stack(int *array, int max)
+static t_stack	*get_stack(int *array, int max)
 {
-	t_list	*head;
-	t_list	*current;
+	t_stack	*head;
+	t_stack	*current;
 	int		index;
 
 	index = 1;
-	head = ft_lstnew(&array[0]);
+	head = ft_stack_create(&array[0]);
 	if (!head)
 		return (NULL);
 	current = head;
 	while (index < max)
 	{
-		current->next = ft_lstnew(&array[index]);
+		current->next = ft_stack_create(&array[index]);
 		if (!current->next)
 		{
-			ft_lstclear(&head, NULL);
+			ft_stack_delete(&head, NULL);
 			return (NULL);
 		}
 		current = current->next;
@@ -91,33 +81,55 @@ static t_list	*get_stack(int *array, int max)
 	return (head);	
 }
 
+static int	is_ordered(t_stack *head)
+{
+	int prev;
+	int current;
+
+	prev = *(int*)head->content;
+	head = head->next;
+	while (head)
+	{
+		current = *(int*)head->content;
+		if (!current)
+			return (0);
+		if (prev > current)
+			return (0);
+		head = head->next;
+		prev = current;
+	}
+	return (1);
+}
+
+void	test(t_stack *stack)
+{
+	sa
+}
+
 int	main(int argc, char *argv[])
 {
 	int		*array;
-	t_list	*stack;
+	t_stack	*stack;
 
-	//atexit(leaks);
+	atexit(leaks);
 
 	if (argc < 2)
 		return (1);
 	array = check_arguments(argc, argv);
 	if (!array)
-		return (printf("Error de argumentos\n"), 1);
-	
-	printf("Array:\n");
-	for (int i = 0; i < argc - 1; i++)
-		printf("\t> %d (%d)\n", array[i], i);
+		return (printf("Error.\n"), 1);
 	stack = get_stack(array, argc - 1);
 	if (!stack)
 		return (printf("Error con la stack :/\n"), 1);
-
-	//ft_lstprint(stack, int_print);
-	/*
-		...
-		Funciones de algoritmos
-		...
-	*/
-	free(array);
-	ft_lstclear(&stack, NULL);
+	ft_stack_updateindex(stack);
+	/*ft_stack_print(stack, int_print);
+	if (!is_ordered(stack))
+	{
+		//manage_order(argc, stack);
+		printf("No ordenaddo\n");
+	}
+	ft_stack_delete(&stack, NULL);
+	free(array);*/
+	test(stack);
 	return (0);
 }
