c1395819e4428c86df0a9b949ce3c4f1cf28cfe4
Luis Ignacio Pastor Cristobal <lpastor-@c2r9s5.42madrid.com>
2023-10-11 12:50:46 +0200

    Updated stack functions (same than lists)

--- include/stack.h
+++ include/stack.h
@@ -4,19 +4,14 @@
 # include <stdlib.h>
 
 typedef struct s_stack {
-	size_t index;
-	void	*content;
+	void			*content;
 	struct s_stack	*next;
-	struct s_stack	*prev;
 } t_stack;
 
-t_stack	*ft_stack_create(void *content);
-void	ft_stack_delete(t_stack **head, void (*delete)(void*));
-void	ft_stack_updateindex(t_stack *head);
+t_stack	*ft_stacknew(void *content);
+void	ft_stackclear(t_stack **lst, void (*del)(void*));
+void	ft_stackadd_back(t_stack **lst, t_stack *new);
+void	ft_stackadd_front(t_stack **lst, t_stack *new);
 void	ft_stack_print(t_stack *stack, void (*print)(void*));
-void	ft_stack_addfront(t_stack **stack, t_stack *node);
-void	ft_stack_addback(t_stack **stack, t_stack *node);
-t_stack	*ft_stack_popfront(t_stack **stack);
-t_stack	*ft_stack_popback(t_stack **stack);
 
 #endif
--- src/t_stack.c
+++ src/t_stack.c
@@ -1,51 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_stack.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/06 08:30:32 by lpastor-          #+#    #+#             */
+/*   Updated: 2023/10/11 11:42:19 by lpastor-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/stack.h"
 
 #include <stdio.h>
 
-t_stack	*ft_stack_create(void *content)
-{
-	t_stack	*node;
 
-	node = (t_stack*) malloc(sizeof(t_stack));
-	if (!node)
+t_stack	*ft_stacknew(void *content)
+{
+	t_stack	*memory;
+
+	memory = (t_stack *) malloc(sizeof(t_stack));
+	if (!memory)
 		return (NULL);
-	node->content = content;
-	node->index = 0;
-	node->next = NULL;
-	node->prev = NULL;
-	return (node);
+	memory->content = content;
+	memory->next = NULL;
+	return (memory);
 }
 
-void	ft_stack_delete(t_stack **head, void (*delete)(void*))
+void	ft_stackclear(t_stack **lst, void (*del)(void*))
 {
+	t_stack	*current;
 	t_stack	*aux;
 
-	if (!head)
+	if (!lst || !(*lst))
 		return ;
-	while (*head)
+	current = *lst;
+	while (current)
 	{
-		aux = *head;
-		*head = (*head)->next;
-		if (delete)
-			delete(aux->content);
+		aux = current;
+		current = current->next;
+		if (del)
+			del(aux->content);
 		free(aux);
 	}
-	*head = NULL;
+	*lst = NULL;
 }
 
-void	ft_stack_updateindex(t_stack *head)
+void	ft_stackadd_back(t_stack **lst, t_stack *new)
 {
-	size_t	index;
-	if (!head)
+	t_stack	*current;
+
+	if (!lst || !new)
 		return ;
-	index = 0;
-	while (head)
+	current = *lst;
+	if (!current)
 	{
-		head->index = index;
-		index++;
-		head = head->next;
+		*lst = new;
+		return ;
 	}
+	while (current->next)
+		current = current->next;
+	current->next = new;
 }
+
+void	ft_stackadd_front(t_stack **lst, t_stack *new)
+{
+	if (!lst || !new)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
+
+
 
 void	ft_stack_print(t_stack *stack, void (*print)(void*))
 {
@@ -56,76 +82,10 @@ void	ft_stack_print(t_stack *stack, void (*print)(void*))
 	count = 0;
 	while (stack)
 	{
-		printf("(Node %zu): ", stack->index);
+		printf("(Node %zu): ", count);
 		print(stack->content);
 		printf("\n");
 		stack = stack->next;
 		count++;
 	}
-}
-
-void	ft_stack_addfront(t_stack **stack, t_stack *node)
-{
-	if (!stack || !node)
-		return ;
-	node->next = *stack;
-	(*stack)->prev = node;
-	*stack = node;
-}
-
-void	ft_stack_addback(t_stack **stack, t_stack *node)
-{
-	t_stack	*current;
-
-	if (!stack || !node)
-		return ;
-	current = *stack;
-	if (!current)
-	{
-		*stack = node;
-		return ;
-	}
-	while (current->next)
-		current = current->next;
-	current->next = node;
-	node->prev = current;
-}
-
-t_stack	*ft_stack_popfront(t_stack **stack)
-{
-	t_stack	*data;
-
-	if (!stack || !(*stack))
-		return (NULL);
-
-	// Cogemos el primer elemento
-	data = (*stack);
-
-	// Cambiamos la primera posicion
-	*stack = (*stack)->next;
-	data->next = NULL;
-	if (*stack)
-		(*stack)->prev = NULL;
-	return (data);
-}
-
-t_stack	*ft_stack_popback(t_stack **stack)
-{
-	t_stack *current;
-
-	if (!stack || !(*stack))
-		return (NULL);
-	if (!(*stack)->next)
-	{
-		current = *stack;
-		current->next = NULL;
-		*stack = NULL;
-		return (current);
-	}
-	current = *stack;
-	while (current->next)
-		current = current->next;
-	current->prev->next = NULL;
-	current->prev = NULL;
-	return (current);
 }
