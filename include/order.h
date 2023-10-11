c15bbb6e847aae70c7c3856b16ab4cd7a69e047c
Luis Ignacio Pastor Cristobal <lpastor-@c2r9s5.42madrid.com>
2023-10-11 12:50:05 +0200

    Added functions to short 2 and 3 elements

---
+++ include/order.h
@@ -0,0 +1,8 @@
+#ifndef ORDER_H
+# define ORDER_H
+
+# include "stack.h"
+
+void	manage_order(t_stack **stack, int count);
+
+#endif
--- src/order.c
+++ src/order.c
@@ -1,18 +1,38 @@
-#include "../include/stack.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   order.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 08:07:44 by lpastor-          #+#    #+#             */
+/*   Updated: 2023/10/11 11:59:55 by lpastor-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void	order2(t_stack *stack)
+#include "../include/stack.h"
+#include "../include/instructions.h"
+
+void	order3(t_stack **head)
 {
-	
+	t_stack	*stack = *head;
+	if (*(int*)stack->content > *(int*)stack->next->content && \
+		*(int*)stack->content > *(int*)stack->next->next->content)
+		r(head, 'a');
+	else if (*(int*)stack->next->content > *(int*)stack->content && \
+		*(int*)stack->next->content > *(int*)stack->next->next->content)
+		rr(head, 'a');
+	if (*(int*)stack->content > *(int*)stack->next->content)
+		s(head, 'a');
 }
 
-void	manage_order(int argc, t_stack *stack)
+void	manage_order(t_stack **stack, int argc)
 {
 	if (!stack)
 		return ;
 	if (argc == 2)
-		order2(stack);
+		s(stack, 'a');
 	else if (argc == 3)
 		order3(stack);
-	
 	return ;
 }
