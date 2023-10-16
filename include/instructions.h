7f43beb764c7a6e8a86418ee0dceb2ccd533f3a8
Luis Ignacio Pastor Cristobal <lpastor-@c2r9s5.42madrid.com>
2023-10-16 14:36:55 +0200

    Updated instructions header file

--- include/instructions.h
+++ include/instructions.h
@@ -3,10 +3,15 @@
 
 # include "stack.h"
 
-void	s(t_stack **stack, char letter);
-void	p(t_stack **origen, t_stack **dest, char letter);
-void	r(t_stack **stack, char letter);
-void	rr(t_stack **stack, char letter);
+void	sa(t_stack **stack_a);
+void	pa(t_stack **stack_b, t_stack **stack_a);
+void	ra(t_stack **stack_a);
+void	rra(t_stack **stack_a);
+
+void	sb(t_stack **stack_b);
+void	pb(t_stack **stack_a, t_stack **stack_b);
+void	rb(t_stack **stack_b);
+void	rrb(t_stack **stack_b);
 
 
 
