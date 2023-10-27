53f1e0c9df9f5254aedb85dbda194b6064c94a82
Luis Ignacio Pastor Cristobal <lpastor-@c2r9s5.42madrid.com>
2023-10-27 13:53:15 +0200

    Deleted divided functions

--- include/order.h
+++ include/order.h
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   order.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 10:44:39 by lpastor-          #+#    #+#             */
+/*   Updated: 2023/10/25 12:09:30 by lpastor-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ORDER_H
 # define ORDER_H
 
 # include "stack.h"
 
 int		is_ordered(t_stack	*stack);
-void	manage_order(t_stack **stack, int count);
+void	manage_order(t_stack **stack);
 
 #endif
