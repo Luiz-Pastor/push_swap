995f10a42f6529547daab6405f073ad653725af2
Luis Ignacio Pastor Cristobal <lpastor-@c2r9s5.42madrid.com>
2023-10-27 13:49:37 +0200

    Updated basic cases

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 08:30:32 by lpastor-          #+#    #+#             */
-/*   Updated: 2023/10/17 10:00:36 by lpastor-         ###   ########.fr       */
+/*   Updated: 2023/10/27 08:13:52 by lpastor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,66 +18,23 @@
 #include "../include/order.h"
 #include "../include/colores.h"
 #include "../include/arguments.h"
+#include "../include/utils.h"
 
-void leaks(void)
-{
-	system("leaks -q push_swap");
-}
-
-void	int_print(void *elem)
-{
-	if (!elem)
-		return ;
-	printf("%d", *(int*)elem);
-}
-
-
-void test(t_stack **stack_a)
-{
-	t_stack	*stack_b = NULL;
-
-	sa(stack_a);
-	
-	pb(stack_a, &stack_b);
-	pb(stack_a, &stack_b);
-	pb(stack_a, &stack_b);
-	
-	ra(stack_a);
-	rb(&stack_b);
-	
-	rra(stack_a);
-	rrb(&stack_b);
-	
-	sa(stack_a);
-	
-	pa(&stack_b, stack_a);
-	pa(&stack_b, stack_a);
-	pa(&stack_b, stack_a);
-
-	ft_stack_print(*stack_a, int_print);
-	printf("\n");
-	ft_stack_print(stack_b, int_print);
-
-	is_ordered(*stack_a) == 0 ? printf("No ordenada") : printf("Ordenada");
-}
+/* 
+- Salida de errores: 2
+*/
 
 int	main(int argc, char *argv[])
 {
 	t_stack	*stack;
 
-	//printf("> %d\n", argc);
-	//atexit(leaks);
 	if (argc < 2)
-		return (1);
+		return (fprintf(stdout, "Error\n"), 1);
 	stack = manage_arguments(argc, argv);
 	if (!stack)
-		return (printf("Error.\n"), 1);
-	ft_stack_print(stack, int_print);
-	//test(&stack);
-	
+		return (fprintf(stdout, "Error\n"), 1);
 	if (!is_ordered(stack))
-		manage_order(&stack, argc - 1);
-
+		manage_order(&stack);
 	ft_stackclear(&stack, free);
 	return (0);
 }
--- src/order.c
+++ src/order.c
@@ -6,145 +6,98 @@
 /*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 08:07:44 by lpastor-          #+#    #+#             */
-/*   Updated: 2023/10/17 08:13:38 by lpastor-         ###   ########.fr       */
+/*   Updated: 2023/10/27 08:16:45 by lpastor-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "../include/stack.h"
 #include "../include/instructions.h"
+#include "../include/utils.h"
 
 /*static void int_print(void *elem)
 {
 	printf("%d", *(int*)elem);
 }*/
 
-int	is_ordered(t_stack	*stack)
-{
-	int	prev;
-	int current;
-
-	if (!stack)
-		return (1);
-	prev = *(int*)stack->content;
-	stack = stack->next;
-	while (stack)
-	{
-		current = *(int*)stack->content;
-		if (prev > current)
-			return (0);
-		prev = current;
-		stack = stack->next;
-	}
-	return (1);
-}
-
-int	get_max_element(t_stack *head)
-{
-	int	max;
-	
-	max = *(int*)head->content;
-	head = head->next;
-	while (head != NULL)
-	{
-		if (*(int*)head->content > max)
-			max = *(int*)head->content;
-		head = head->next;
-	}
-	return (max);
-}
-
-int	get_min_element(t_stack *head)
-{
-	int	min;
-	
-	min = *(int*)head->content;
-	head = head->next;
-	while (head != NULL)
-	{
-		if (*(int*)head->content < min)
-			min = *(int*)head->content;
-		head = head->next;
-	}
-	return (min);
-}
-
 void	order3(t_stack **head)
 {
-	int max;
+	int	max;
 
 	max = get_max_element(*head);
-	if (*(int*)(*head)->content == max)
-		ra(head);
-	else if (*(int*)(*head)->next->content ==	 max)
-		rra(head);
-	if (*(int*)(*head)->content > *(int*)(*head)->next->content)
-		sa(head);
+	if (*(int *)(*head)->content == max)
+		ra(head, 1);
+	else if (*(int *)(*head)->next->content == max)
+		rra(head, 1);
+	if (*(int *)(*head)->content > *(int *)(*head)->next->content)
+		sa(head, 1);
 }
 
 void	order4(t_stack **head)
 {
 	t_stack	*stack_b;
-	int min;
+	int		min;
 
 	stack_b = NULL;
 	min = get_min_element(*head);
-	if (*(int*)(*head)->next->content == min)
-		ra(head);
-	else if (*(int*)(*head)->next->next->content == min)
+	if (*(int *)(*head)->next->content == min)
+		ra(head, 1);
+	else if (*(int *)(*head)->next->next->content == min)
 	{
-		ra(head);
-		ra(head);
+		ra(head, 1);
+		ra(head, 1);
 	}
-	else if (*(int*)(*head)->next->next->next->content == min)
-		rra(head);
-	
+	else if (*(int *)(*head)->next->next->next->content == min)
+		rra(head, 1);
 	if (is_ordered(*head))
 		return ;
-	pb(head, &stack_b);
+	pb(head, &stack_b, 1);
 	order3(head);
-	pa(&stack_b, head);
+	pa(&stack_b, head, 1);
 }
 
 void	order5(t_stack **head)
 {
-	t_stack *stack_b;
-	int min;
+	t_stack	*stack_b;
+	int		min;
 
 	stack_b = NULL;
 	min = get_min_element(*head);
-	if (*(int*)(*head)->next->content == min)
-		ra(head);
-	else if (*(int*)(*head)->next->next->content == min)
+	if (*(int *)(*head)->next->content == min)
+		ra(head, 1);
+	else if (*(int *)(*head)->next->next->content == min)
 	{
-		ra(head);
-		ra(head);
+		ra(head, 1);
+		ra(head, 1);
 	}
-	else if (*(int*)(*head)->next->next->next->content == min)
+	else if (*(int *)(*head)->next->next->next->content == min)
 	{
-		rra(head);
-		rra(head);
+		rra(head, 1);
+		rra(head, 1);
 	}
-	else if (*(int*)(*head)->next->next->next->next->content == min)
-		rra(head);
+	else if (*(int *)(*head)->next->next->next->next->content == min)
+		rra(head, 1);
 	if (is_ordered(*head))
 		return ;
-	pb(head, &stack_b);
+	pb(head, &stack_b, 1);
 	order4(head);
-	pa(&stack_b, head);
+	pa(&stack_b, head, 1);
 }
 
-void	manage_order(t_stack **stack, int argc)
+void	manage_order(t_stack **stack)
 {
+	int	arguments;
+
+	arguments = ft_stacksize(*stack);
 	if (!stack)
 		return ;
-	if (argc == 2)
-		sa(stack);
-	else if (argc == 3)
+	if (arguments == 2)
+		sa(stack, 1);
+	else if (arguments == 3)
 		order3(stack);
-	else if (argc == 4)
+	else if (arguments == 4)
 		order4(stack);
-	else if (argc == 5)
+	else if (arguments == 5)
 		order5(stack);
 	else
 		return ;
