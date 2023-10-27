6664fd1d8e2c2c24350bd6f9a0389e5c2fe6e193
Luis Ignacio Pastor Cristobal <lpastor-@c2r9s5.42madrid.com>
2023-10-27 13:51:35 +0200

    Updated stack header

--- include/stack.h
+++ include/stack.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 10:45:13 by lpastor-          #+#    #+#             */
+/*   Updated: 2023/10/24 10:45:16 by lpastor-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef STACK_H
 # define STACK_H
 
@@ -6,12 +18,14 @@
 typedef struct s_stack {
 	void			*content;
 	struct s_stack	*next;
-} t_stack;
+	struct s_stack	*prev;
+}	t_stack;
 
 t_stack	*ft_stacknew(void *content);
 void	ft_stackclear(t_stack **lst, void (*del)(void*));
 void	ft_stackadd_back(t_stack **lst, t_stack *new);
 void	ft_stackadd_front(t_stack **lst, t_stack *new);
+int		ft_stacksize(t_stack *lst);
 void	ft_stack_print(t_stack *stack, void (*print)(void*));
 
 #endif
