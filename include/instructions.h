2dad80606a165892f4a4d6f08c72b1ccfd5d1500
Luis Ignacio Pastor Cristobal <lpastor-@c2r9s5.42madrid.com>
2023-10-27 13:52:34 +0200

    Replaced the functions

--- include/instructions.h
+++ include/instructions.h
@@ -1,18 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   instructions.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 10:44:30 by lpastor-          #+#    #+#             */
+/*   Updated: 2023/10/24 10:44:32 by lpastor-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef INSTRUCTIONS_H
 # define INSTRUCTIONS_H
 
 # include "stack.h"
 
-void	sa(t_stack **stack_a);
-void	pa(t_stack **stack_b, t_stack **stack_a);
-void	ra(t_stack **stack_a);
-void	rra(t_stack **stack_a);
+int	sa(t_stack **stack_a, int print);
+int	pa(t_stack **stack_b, t_stack **stack_a, int print);
+int	ra(t_stack **stack_a, int print);
+int	rra(t_stack **stack_a, int print);
 
-void	sb(t_stack **stack_b);
-void	pb(t_stack **stack_a, t_stack **stack_b);
-void	rb(t_stack **stack_b);
-void	rrb(t_stack **stack_b);
-
-
+int	sb(t_stack **stack_b, int print);
+int	pb(t_stack **stack_a, t_stack **stack_b, int print);
+int	rb(t_stack **stack_b, int print);
+int	rrb(t_stack **stack_b, int print);
 
 #endif
