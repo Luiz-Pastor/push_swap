dc8549c9919b0e3649bf55817ee9ef68cd615bb2
Luiz-Pastor <luigpacr5@gmail.com>
2023-10-28 23:41:40 +0200

    Attributes added to structure 't_stack'

--- include/stack.h
+++ include/stack.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   stack.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 10:45:13 by lpastor-          #+#    #+#             */
-/*   Updated: 2023/10/24 10:45:16 by lpastor-         ###   ########.fr       */
+/*   Updated: 2023/10/28 19:24:41 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@
 # include <stdlib.h>
 
 typedef struct s_stack {
+	int				index;
+	int				pos_value;
+	int				target;
+	int				cost_a;
+	int				cost_b;
+
 	void			*content;
 	struct s_stack	*next;
 	struct s_stack	*prev;
