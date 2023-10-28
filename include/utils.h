0c47b0bf048873d200861ae222e3e035980ae1c5
Luiz-Pastor <luigpacr5@gmail.com>
2023-10-28 23:42:32 +0200

    Added functions

--- include/utils.h
+++ include/utils.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 08:12:47 by lpastor-          #+#    #+#             */
-/*   Updated: 2023/10/27 09:16:36 by lpastor-         ###   ########.fr       */
+/*   Updated: 2023/10/28 23:22:03 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@
 
 int	is_ordered(t_stack	*stack);
 int	get_min_element(t_stack *head);
+int	get_min_index(t_stack *head);
 int	get_max_element(t_stack *head);
 void	*int_copy(int elem);
-void	*ft_charmatrix_free(void **elem);
+void	*ft_charmatrix_free(char **elem);
+void int_print(void *elem);
 
 #endif
