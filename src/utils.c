c439db7eb4d63c2a6ec530b8016fe5d093910302
Luiz-Pastor <luigpacr5@gmail.com>
2023-10-28 23:40:48 +0200

    Added common functions

--- src/utils.c
+++ src/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpastor- <lpastor-@student.42madrid>       +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 08:11:19 by lpastor-          #+#    #+#             */
-/*   Updated: 2023/10/27 09:16:16 by lpastor-         ###   ########.fr       */
+/*   Updated: 2023/10/28 23:36:48 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,16 +47,39 @@ int	get_min_element(t_stack *head)
 	return (min);
 }
 
+int	get_min_index(t_stack *head)
+{
+	int	min;
+	int min_index;
+	int iterate_index;
+
+	min = *(int *)head->content;
+	min_index = 0;
+	iterate_index = 0;
+	head = head->next;
+	while (head != NULL)
+	{
+		if (*(int *)head->content < min)
+		{
+			min = *(int *)head->content;
+			min_index = iterate_index;
+		}
+		head = head->next;
+		iterate_index++;
+	}
+	return (min_index);
+}
+
 int	get_max_element(t_stack *head)
 {
 	int	max;
 
-	max = *(int *)head->content;
+	max = *(int *)(head->content);
 	head = head->next;
-	while (head != NULL)
+	while (head)
 	{
-		if (*(int *)head->content > max)
-			max = *(int *)head->content;
+		if (*(int *)(head->content) > max)
+			max = *(int *)(head->content);
 		head = head->next;
 	}
 	return (max);
@@ -73,11 +96,18 @@ void	*int_copy(int elem)
 	return (cpy);
 }
 
-void	*ft_charmatrix_free(void **elem)
+void	*ft_charmatrix_free(char **elem)
 {
+	char **head;
+
 	if (!elem)
 		return (NULL);
+	head = elem;
 	while (*elem)
+	{
 		free(*elem);
+		elem++;
+	}
+	free(head);
 	return (NULL);
 }
